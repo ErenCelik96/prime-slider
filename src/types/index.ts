45bec8ffe81068adc48d91ea49e08294348665e6
ErenCelik96 <celik.erenn96@gmail.com>
2025-05-27 15:46:20 +0300

    feat: edited type file

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,7 +1,43 @@
+import "@mui/material/styles";
+
 export interface Photo {
-  albumId: number;
-  id: number;
-  title: string;
+  id: string;
+  author: string;
+  width: number;
+  height: number;
   url: string;
-  thumbnailUrl: string;
+  download_url: string;
+}
+
+declare module "@mui/material/styles" {
+  interface Theme {
+    customComponents: {
+      photoSlider: {
+        overlay: {
+          background: string;
+          textShadow: string;
+          borderRadius: number;
+        };
+        controls: {
+          background: string;
+          hoverBackground: string;
+        };
+      };
+    };
+  }
+  interface ThemeOptions {
+    customComponents?: {
+      photoSlider?: {
+        overlay?: {
+          background?: string;
+          textShadow?: string;
+          borderRadius?: number;
+        };
+        controls?: {
+          background?: string;
+          hoverBackground?: string;
+        };
+      };
+    };
+  }
 }
