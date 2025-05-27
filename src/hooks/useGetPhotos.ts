31bb61c2e5d5d5b42cd9ef53afcf0b0c34c568e5
ErenCelik96 <celik.erenn96@gmail.com>
2025-05-27 16:41:30 +0300

    update: useGetPhotos hook

--- src/hooks/useGetPhotos.ts
+++ src/hooks/useGetPhotos.ts
@@ -5,12 +5,11 @@ export const usePhotos = () => {
   return useQuery({
     queryKey: ["photos"],
     queryFn: async (): Promise<Photo[]> => {
-      const response = await fetch(
-        "https://jsonplaceholder.typicode.com/photos?_limit=20"
-      );
+      const response = await fetch(`${import.meta.env.VITE_API_URL}/list`);
       if (!response.ok) throw new Error("Görseller çekilemedi");
       return response.json();
     },
     staleTime: 1000 * 60 * 5,
+    gcTime: 1000 * 60 * 10,
   });
 };
